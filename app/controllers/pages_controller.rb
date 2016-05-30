9ebf91e35ff99ccae3253e99bc1a1dc9289e0859
justin <justinrmegahan@gmail.com>
2016-05-30 13:28:45 -0700

    Ten random cards on the homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
 	def home
-		@cards = Card.all
+		randomCards = (1..1+Card.all.length).to_a.sort{ rand() - 0.5 }[0..9]
+		@cards = [
+			Card.find(randomCards[0]),
+			Card.find(randomCards[1]),
+			Card.find(randomCards[2]),
+			Card.find(randomCards[3]),
+			Card.find(randomCards[4]),
+			Card.find(randomCards[5]),
+			Card.find(randomCards[6]),
+			Card.find(randomCards[7]),
+			Card.find(randomCards[8]),
+			Card.find(randomCards[9])]
 	end
 end
