9a1c5b2f98fd25e3eda55f8c6c51c390928cd41b
justin <justinrmegahan@gmail.com>
2016-05-29 17:34:44 -0700

    Cards create, edit, show views

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,43 @@
+class CardsController < ApplicationController
+	
+	def index
+		@cards = Card.all
+	end
+
+	def new
+		@card = Card.new
+	end
+
+	def edit
+		@card = Card.find(params[:id])
+	end
+
+	def update
+		@card = Card.find(params[:id])
+		if @card.update(card_params)
+			flash[:notice] = "Card updated"
+			redirect_to card_path(@card)
+		else
+			render :new
+		end
+	end
+
+	def create
+		@card = Card.new(card_params)
+		if @card.save
+			flash[:notice] = "Card created"
+			redirect_to card_path(@card)
+		else
+			render :new
+		end
+	end
+
+	def show
+		@card = Card.find(params[:id])
+	end
+
+	private
+	def card_params
+		params.require(:card).permit(:question, :answer, :notes)
+	end
+end
